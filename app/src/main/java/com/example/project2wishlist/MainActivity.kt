8c7c4bc21ad5a30463730e5c61e786bd098d92f6
Arif Nasif <arif.nasif25@myhunter.cuny.edu>
2022-09-24 01:00:38 -0400

    git commit push

--- app/src/main/java/com/example/project2wishlist/MainActivity.kt
+++ app/src/main/java/com/example/project2wishlist/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var myWishList : List<ListItem>
+     var myWishList = ArrayList<ListItem>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listsRv = findViewById<RecyclerView>(R.id.Listrv)
-        val adapter = WishListAdapter(this, myWishList)
+        val adapter = WishListAdapter( myWishList)
 
         val itemInput = findViewById<EditText>(R.id.itemID)
         val priceInput = findViewById<EditText>(R.id.priceID)
         val urlInput = findViewById<EditText>(R.id.urlID)
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            myWishList.add(ListItem(itemInput.text.toString(), urlInput.text.toString(), priceInput.text.toString()))
+           myWishList.add(ListItem(itemInput.text.toString(), urlInput.text.toString(), priceInput.text.toString()))
             adapter.notifyDataSetChanged()
         }
         listsRv.adapter=adapter
